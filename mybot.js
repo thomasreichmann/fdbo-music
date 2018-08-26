6d6ace0125f3472ad9e449820e33cb6f7a3c7a62
Thomas Reichmann <thomasreichmann@hotmail.com>
2018-08-26 04:34:07 -0300

    Adicionando Activity e testando novos comandos

--- mybot.js
+++ mybot.js
@@ -1,11 +1,38 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json")
+const config = require("./config.json");
 
-const fs = require("fs")
+const fs = require("fs");
+
+function writecfg() {
+  fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
+};
+
+function calculateGuilds() {
+
+  let guilds = client.guilds;
+
+  let guildCount = client.guilds.size;
+  let userCount = 0;
+
+  guilds.forEach(g => {
+    userCount += g.memberCount;
+  });
+
+  return [guildCount, userCount];
+}
+
+function refreshActivity() {
+
+  let [guildCount, userCount] = calculateGuilds()
+
+  client.user.setActivity(`${guildCount} servers, com ${userCount} users no total!`);
+}
 
 client.on("ready", () => {
   console.log(`Pronto!`);
+
+  refreshActivity()
 });
 
 client.on("message", (message) => {
@@ -33,18 +60,38 @@ client.on("message", (message) => {
         message.channel.send("Prefixo mudado de: " + prefix + " Para: " + newPrefix)
 
         config.prefix = newPrefix
-        fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
+        writecfg()
+        //fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
       }
 
       break;
 
-      case "help":
+    case "mandarmsg":
+      let target = message.mentions.members.first()
+      let msg = args[1]
+
+      target.send(`${message.author} me mandou te falar isso:
+      ${msg}`)
+      break;
+
+      // case "setgame":
+      //   let activity = args[0]
+      //   config.lastActivity = activity
+
+      //   client.user.setActivity(`${activity}`)
+      //   message.reply(`Agora estou jogando ${activity}!`)
+
+      //   writecfg()
+      //   break;
+
+    case "help":
       message.channel.send(`Toma os comando ae:
       ${prefix}ping - Ele reponde
       ${prefix}prefix (novo prefixo) - Muda o prefixo`)
+
   }
 
-  console.log(`O ping e igual a: ${client.ping}`)
+  console.log(`O usuario ${message.author.tag}(${message.author}) executou o comando ${command}, com ms = ${client.ping}`)
 
   // console.log("Mensagem recebida, conteudo: " + message.content + ", do usuario: " + message.member.user.tag)
 
