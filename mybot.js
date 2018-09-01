d875282dc741a0e7733b96bd3e2844187b67ec0a
Thomas Reichmann <thomasreichmann@hotmail.com>
2018-09-01 01:53:01 -0300

    Upd

--- mybot.js
+++ mybot.js
@@ -1,8 +1,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const yt = require('ytdl-core')
+const Youtube = require('simple-youtube-api')
+const fs = require("fs");
+
 const config = require("./config.json");
 
-const fs = require("fs");
+const youtube = new Youtube(config.youtubeApiKey)
 
 function writecfg() {
   fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
@@ -38,11 +42,9 @@ client.on("ready", () => {
   setTimeout(refreshActivity, 36e+6)
 });
 
-client.on("message", (message) => {
+client.on("message", async message => {
 
-  if (!message.content.startsWith(config.prefix) || message.author.bot) {
-    return
-  }
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
@@ -56,6 +58,8 @@ client.on("message", (message) => {
 
     case "prefix":
       let newPrefix = args[0]
+
+      Music.updatePrefix = newPrefix
 
       if (newPrefix != null) {
 
@@ -71,10 +75,68 @@ client.on("message", (message) => {
 
     case "mandarmsg":
       let target = message.mentions.members.first()
-      let msg = args[1]
+      let msg = args
+      msg.shift()
 
       target.send(`${message.author} me mandou te falar isso:
-      ${msg}`)
+      ${msg.join(" ")}`)
+      break;
+
+    case "kick":
+      {
+        let user = message.mentions.users.first();
+        let member = message.guild.member(user);
+
+        if (message.member.permissions.has("KICK_MEMBERS", true)) {
+          member.kick().then(() => {
+
+            message.reply(`O membro <@${member.id}> foi kickado!`)
+
+          }).catch(err => {
+
+            message.reply(`Nao foi possivel kickar o usuario: ${err}`)
+
+          })
+        } else {
+          message.reply(`Voce nao possui as permissoes necessarias para utilizar esse comando`)
+        }
+
+      }
+
+      break;
+
+    case "play":
+
+      let channel
+      channel = message.member.voiceChannel
+
+      let video = await youtube.getVideo(args[0]).catch(err => console.log(err))
+
+      channel.join().then(connection => {
+
+          let song = {
+            url: video.url,
+            name: video.title
+          }
+
+          let streamOptions = {
+            volume: 1,
+          }
+
+          const stream = yt(song.url, streamOptions)
+          let dispatcher = connection.playStream(stream)
+
+          message.reply(`
+        Tocando: ${song.name}
+        URL: ${song.url}
+        `)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+
+        
+
       break;
 
     case "help":
